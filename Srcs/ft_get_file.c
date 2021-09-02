ee383100d7e6754c4b36a215b71a6b2af66ec0a6
user42 <duck.marini@gmail.com>
2021-09-02 12:13:02 +0200

    added protection of empty file

--- Srcs/ft_get_file.c
+++ Srcs/ft_get_file.c
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 14:13:03 by amarini-          #+#    #+#             */
-/*   Updated: 2021/08/19 12:38:19 by amarini-         ###   ########.fr       */
+/*   Updated: 2021/08/31 19:03:59 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,25 @@
 int	ft_count_lines(char *path)
 {
 	int		result;
-	int		read;
+	int		has_read;
 	int		fd;
 	char	*line;
 
 	result = 0;
-	read = 1;
+	has_read = 1;
+	line = ft_strdup("");
 	fd = open(path, O_RDONLY);
-	while (read > 0 && read != -1)
+	if (read(fd, line, 1) <= 0)
 	{
-		read = get_next_line(fd, &line);
+		free(line);
+		return (-1);
+	}
+	free(line);
+	close(fd);
+	fd = open(path, O_RDONLY);
+	while (has_read > 0 && has_read != -1)
+	{
+		has_read = get_next_line(fd, &line);
 		free(line);
 		result++;
 	}
@@ -43,6 +52,8 @@ int	ft_get_file(char *path, char ***content)
 	read = 1;
 	length = ft_count_lines(path);
 	fd = open(path, O_RDONLY);
+	if (length < 0)
+		return (-1);
 	(*content) = (char **)malloc((length + 1) * sizeof(char *));
 	if (!(*content))
 		return (-1);
